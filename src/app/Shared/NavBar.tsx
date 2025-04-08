f9c99af6f5477ab835cf91784d294352fcaee85c
Kutub Udd <socialnetwork98710@gmail.com>
2025-04-08 12:14:33 +0600

    added

--- src/app/Shared/NavBar.tsx
+++ src/app/Shared/NavBar.tsx
@@ -13,7 +13,7 @@ import {
 import { Button } from "@/components/ui/button";
 import clsx from "clsx";
 import AnimatedButton from "@/components/ui/AnimateButton";
-import { useState } from "react";
+// import { useState } from "react";
 
 const navLinks = [
   { label: "Home", href: "/" },
@@ -24,7 +24,8 @@ const navLinks = [
 
 const Navbar = () => {
   const pathname = usePathname();
-  const [user, setUser] = useState(false);
+  const user = false;
+  // const [user, setUser] = useState(false);
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-white dark:bg-gray-950">
       <div className="flex h-16 items-center justify-between px-4 md:px-6 max-w-7xl mx-auto">
